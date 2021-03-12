1fb5b9619b6632cf601c3f99c0dc6029d8a95590
LukmanSingla <79403472+LukmanSingla@users.noreply.github.com>
2021-03-12 18:45:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -55,7 +55,7 @@ Engine.update(engine);
 }
 
 function keyPressed() {
- if (mouseWentDown("left")) {
+ if (keyCode === DOWN_ARROW) {
     // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
     Body.setStatic(packageBody,false);
 
